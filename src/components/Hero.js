f19921a66d8a70a7ab2ab92d3710d4d7430ecc03
Guilherme Rodrigues <gadr90@gmail.com>
2016-05-04 13:24:46 +0200

    Improve fonts and breakpoints

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,15 +5,17 @@ const Hero = React.createClass({
   render() {
     return (
       <div className="hero">
-        <h1 className="hero-title">
-          Torne-se um desenvolvedor de software profissional.
-        </h1>
-        <p className="hero-subtitle">
-          Finalmente, uma plataforma de ensino para o nosso século.
-          Aprenda mais rápido do que nunca e receba ofertas de emprego das melhores empresas do mercado.
-          Qual vai ser o seu caminho? 
-          Estamos animados para descobrir.
-        </p>
+        <div className="hero-container">
+          <h1 className="hero-title">
+            Torne-se um desenvolvedor de software profissional.
+          </h1>
+          <p className="hero-subtitle">
+            Finalmente, uma plataforma de ensino para o nosso século.
+            Aprenda mais rápido do que nunca e receba ofertas de emprego das melhores empresas do mercado.
+            Qual vai ser o seu caminho?
+            Estamos animados para descobrir.
+          </p>
+        </div>
       </div>
     )
   }
